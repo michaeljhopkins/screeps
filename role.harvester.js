7595112f5263aa53f3df481019bc4900a2162e2e
Michael Hopkins <mhopkins321@gmail.com>
2017-02-17 23:25:21 -0700

    upgrading harvester

--- role.harvester.js
+++ role.harvester.js
@@ -1,7 +1,8 @@
 module.exports = {
     // a function to run the logic for this role
+    /** @param {Creep} creep */
     run: function(creep) {
-        // if creep is bringing energy to the spawn or an extension but has no energy left
+        // if creep is bringing energy to a structure but has no energy left
         if (creep.memory.working == true && creep.carry.energy == 0) {
             // switch state
             creep.memory.working = false;
@@ -12,9 +13,9 @@ module.exports = {
             creep.memory.working = true;
         }
 
-        // if creep is supposed to transfer energy to the spawn or an extension
+        // if creep is supposed to transfer energy to a structure
         if (creep.memory.working == true) {
-            // find closest spawn or extension which is not full
+            // find closest spawn, extension or tower which is not full
             var structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
                 // the second argument for findClosestByPath is an object which takes
                 // a property called filter which can be a function
@@ -24,6 +25,10 @@ module.exports = {
                 || s.structureType == STRUCTURE_TOWER)
                 && s.energy < s.energyCapacity
             });
+
+            if (structure == undefined) {
+                structure = creep.room.storage;
+            }
 
             // if we found one
             if (structure != undefined) {
@@ -36,13 +41,7 @@ module.exports = {
         }
         // if creep is supposed to harvest energy from source
         else {
-            // find closest source
-            var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-            // try to harvest energy, if the source is not in range
-            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                // move towards the source
-                creep.moveTo(source);
-            }
+            creep.getEnergy(false, true);
         }
     }
 };
