22c169de228303bb48929222250095432407f53c
Michael Hopkins <mhopkins321@gmail.com>
2017-02-17 23:25:48 -0700

    updating wall repairer

--- role.wallRepairer.js
+++ role.wallRepairer.js
@@ -2,6 +2,7 @@ var roleBuilder = require('role.builder');
 
 module.exports = {
     // a function to run the logic for this role
+    /** @param {Creep} creep */
     run: function(creep) {
         // if creep is trying to repair something but has no energy left
         if (creep.memory.working == true && creep.carry.energy == 0) {
@@ -26,17 +27,12 @@ module.exports = {
             // loop with increasing percentages
             for (let percentage = 0.0001; percentage <= 1; percentage = percentage + 0.0001){
                 // find a wall with less than percentage hits
-
-                // for some reason this doesn't work
-                // target = creep.pos.findClosestByPath(walls, {
-                //     filter: (s) => s.hits / s.hitsMax < percentage
-                // });
-
-                // so we have to use this
-                target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                    filter: (s) => s.structureType == STRUCTURE_WALL &&
-                    s.hits / s.hitsMax < percentage
-                });
+                for (let wall of walls) {
+                    if (wall.hits / wall.hitsMax < percentage) {
+                        target = wall;
+                        break;
+                    }
+                }
 
                 // if there is one
                 if (target != undefined) {
@@ -59,17 +55,9 @@ module.exports = {
                 roleBuilder.run(creep);
             }
         }
-        // if creep is supposed to harvest energy from source
+        // if creep is supposed to get energy
         else {
-            // find closest source
-            var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE, {
-                filter: (s) => s.energy > 0
-            });
-            // try to harvest energy, if the source is not in range
-            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                // move towards the source
-                creep.moveTo(source);
-            }
+            creep.getEnergy(true, true);
         }
     }
 };
